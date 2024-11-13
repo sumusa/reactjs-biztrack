f760dd996fefbdca195695e8e4e190b2e40b83fd
sumusa <physiosummy@gmail.com>
2024-11-13 17:48:13 -0330

    add Settings configuration page

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,13 @@
+import EmptyState from "../components/EmptyState";
+
+const Settings = () => {
+  return (
+    <EmptyState
+      title="Settings & Preferences"
+      message="We're still tweaking the knobs and dials..."
+      image="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjJkMzBkMTYtOTM4ZC00ZmM2LWE0ZTgtNzg4ZjM2MGQ5YzM0JmVwPXYxX2dpZnNfZ2lmJmN0PWc/PKfEGOAGEzuQE/giphy.gif"
+    />
+  );
+};
+
+export default Settings;
