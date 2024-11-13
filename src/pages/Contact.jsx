8077a99e6969921e4f8b9679742c8f81b3e5803a
sumusa <physiosummy@gmail.com>
2024-11-13 17:48:00 -0330

    implement Contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import EmptyState from "../components/EmptyState";
+
+const Contact = () => {
+  return (
+    <EmptyState
+      title="Want to get in touch?"
+      message="Our contact form is coming soon..."
+      image="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjJkMzBkMTYtOTM4ZC00ZmM2LWE0ZTgtNzg4ZjM2MGQ5YzM0JmVwPXYxX2dpZnNfZ2lmJmN0PWc/3o7TKSjRrfIPjeiVyM/giphy.gif"
+    />
+  );
+};
+
+export default Contact;
